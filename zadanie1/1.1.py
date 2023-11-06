4b2c9a773b24ad7cd904e8b87c15fea668e9d41d
sqweeeek <san.shishkina09@gmail.com>
2023-11-07 02:44:57 +0300

    first

---
+++ zadanie1/1.1.py
@@ -0,0 +1,16 @@
+class Car:
+    color = None
+    fuel = None
+
+    def go(self):
+        pass
+    
+    def turn(self):
+        pass
+    
+    def stop(self):
+        pass
+
+myCar = Car()
+myCar.color = "Red"
+myCar.fuel = 50
